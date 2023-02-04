39a74b57270448d763fe5cee561d76acf4e09907
OmarGuajardo <omar.guajardog@outlook.com>
2023-02-03 19:41:28 -0800

    committing

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 import Media
 import openai
 import os
+
 # ChatGpt Class to handle text generation and image generation? 
 # Instagram Class to handle posting, captions and sequence 
 # Class to handle posting, captions and sequence 
