d33cef7484b36b0277f48fa3abe5e1cad90fbc11
OmarGuajardo <omar.guajardog@outlook.com>
2024-03-21 21:03:38 -0700

    made a welcome and login screen

--- main.py
+++ main.py
@@ -1,40 +1,51 @@
 import json
-from flask import Flask, request
+from flask import Flask, request, render_template
 from Media import Media
 from instagrapi import Client
+import schedule
+import time
 import json
 import os
+from threading import Thread
+import datetime
+
 
 # ChatGpt Class to handle text generation and image generation?
 # Instagram Class to handle posting, captions and sequence
 
 # Server global variables
-app = Flask(__name__)
-media = Media("./media")
+App = Flask(__name__)
+MediaClient = Media("./media")
 instagramClient = Client()
-secrets = json.load(open("secrets.json"))
+Secrets = json.load(open("secrets.json"))
 
 
-@app.route("/login")
+@App.route("/")
+def home():
+    return render_template('welcome.html')
+
+@App.route("/login")
 def login():
-    twofac = request.args.get("twofac")
-    if instagramClient.login(secrets["username"], secrets["password"], True, twofac):
-        return "You have logged in!"
-    else:
-        return "We couldn't log you in"
+    return render_template('login.html')
+    # twofac = request.args.get("twofac")
+    # if instagramClient.login(Secrets["username"], Secrets["password"], True, twofac):
+    #     return "You have logged in!"
+    # else:
+    #     return "We couldn't log you in"
 
 
-@app.route("/post")
+@App.route("/post")
 def post():
     imgPath = request.args.get("img_path")
-    storyPosted = instagramClient.photo_upload_to_story("media_portrait/" + imgPath)
+    # storyPosted = instagramClient.photo_upload_to_story("media_portrait/" + imgPath)
+    storyPosted = instagramClient.photo_upload_to_story("cityexploring/" + imgPath)
     if storyPosted:
         return "We posted to your story!"
     else:
         return "We couldn't post your story :("
 
 
-@app.route("/logout")
+@App.route("/logout")
 def logout():
     if instagramClient.logout():
         return "You're logged out!"
@@ -42,10 +53,41 @@ def logout():
         return "We couldn't log you out!"
 
 
-@app.route("/")
-def home():
-    return media.getMedia()
+def job():
+    # Do some work that only needs to happen once...
+    print("=== JOB EXECUTED ===")
+    # return schedule.CancelJob
+
+
+@App.route("/jobs")
+def jobs():
+    return f"The scheduled job is {'running' if schedule.jobs else 'not running'}"
+
+
+@App.route("/start")
+def start():
+    schedule.every().day.at("23:17").do(job)
+    return "job starting"
+
+
+
+@App.route("/media")
+def media():
+    mediaDir = request.args.get("dir")
+    if mediaDir:
+        return MediaClient.getMedia(mediaDir)
+        pass
+    else:
+        return MediaClient.getMedia(None)
+
+
+def runSchedule():
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
 
 if __name__ == "__main__":
-    app.run(port=1234, debug=True)
+    t = Thread(target=runSchedule)
+    t.start()
+    App.run(port=1234, debug=True)
